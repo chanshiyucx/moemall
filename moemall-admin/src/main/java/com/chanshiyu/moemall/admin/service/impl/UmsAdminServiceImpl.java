d1d8aa8da377ffc53f54990da4a94a095e8ce723
chanshiyu <me@chanshiyu.com>
2019-10-07 19:38:08 +0700

    feat: jwt

---
+++ moemall-admin/src/main/java/com/chanshiyu/moemall/admin/service/impl/UmsAdminServiceImpl.java
@@ -0,0 +1,100 @@
+package com.chanshiyu.moemall.admin.service.impl;
+
+import com.chanshiyu.moemall.admin.service.UmsAdminService;
+import com.chanshiyu.moemall.admin.util.JwtTokenUtil;
+import com.chanshiyu.moemall.mbg.mapper.UmsAdminMapper;
+import com.chanshiyu.moemall.mbg.model.UmsAdmin;
+import com.chanshiyu.moemall.mbg.model.UmsPermission;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * UmsAdminService实现类
+ * Created by macro on 2018/4/26.
+ */
+@Service
+public class UmsAdminServiceImpl implements UmsAdminService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UmsAdminServiceImpl.class);
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Value("${jwt.tokenHead}")
+    private String tokenHead;
+
+    @Autowired
+    private UmsAdminMapper adminMapper;
+
+    @Override
+    public UmsAdmin getAdminByUsername(String username) {
+        Example example = new Example(UmsAdmin.class);
+        example.createCriteria().andEqualTo("username", username);
+        List<UmsAdmin> adminList = adminMapper.selectByExample(example);
+        if (adminList != null && adminList.size() > 0) {
+            return adminList.get(0);
+        }
+        return null;
+    }
+
+    @Override
+    public UmsAdmin register(UmsAdmin umsAdminParam) {
+        UmsAdmin umsAdmin = new UmsAdmin();
+        BeanUtils.copyProperties(umsAdminParam, umsAdmin);
+        umsAdmin.setCreateTime(new Date());
+        umsAdmin.setStatus(1);
+        //查询是否有相同用户名的用户
+        Example example = new Example(UmsAdmin.class);
+        example.createCriteria().andEqualTo("username", umsAdmin.getUsername());
+        List<UmsAdmin> umsAdminList = adminMapper.selectByExample(example);
+        if (umsAdminList.size() > 0) {
+            return null;
+        }
+        //将密码进行加密操作
+        String encodePassword = passwordEncoder.encode(umsAdmin.getPassword());
+        umsAdmin.setPassword(encodePassword);
+        adminMapper.insert(umsAdmin);
+        return umsAdmin;
+    }
+
+    @Override
+    public String login(String username, String password) {
+        String token = null;
+        try {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+                throw new BadCredentialsException("密码不正确");
+            }
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            token = jwtTokenUtil.generateToken(userDetails);
+        } catch (AuthenticationException e) {
+            LOGGER.warn("登录异常:{}", e.getMessage());
+        }
+        return token;
+    }
+
+
+    @Override
+    public List<UmsPermission> getPermissionList(Long adminId) {
+        return new ArrayList<UmsPermission>();
+//        return adminRoleRelationDao.getPermissionList(adminId);
+    }
+}
