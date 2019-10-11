9cd82b793c8dd72cb1c5da9aa7c151c6f9baaad8
chanshiyu <me@chanshiyu.com>
2019-10-11 18:30:47 +0700

    feat: 请求日志切面

---
+++ moemall-admin/src/main/java/com/chanshiyu/moemall/admin/dto/bo/WebLog.java
@@ -0,0 +1,68 @@
+package com.chanshiyu.moemall.admin.dto.bo;
+
+import lombok.Data;
+
+/**
+ * @author SHIYU
+ * @date 2019/10/11 18:17
+ * @description Controller层的日志封装类
+ */
+@Data
+public class WebLog {
+
+    /**
+     * 操作描述
+     */
+    private String description;
+
+    /**
+     * 操作用户
+     */
+    private String username;
+
+    /**
+     * 操作时间
+     */
+    private Long startTime;
+
+    /**
+     * 消耗时间
+     */
+    private Integer spendTime;
+
+    /**
+     * 根路径
+     */
+    private String basePath;
+
+    /**
+     * URI
+     */
+    private String uri;
+
+    /**
+     * URL
+     */
+    private String url;
+
+    /**
+     * 请求类型
+     */
+    private String method;
+
+    /**
+     * IP地址
+     */
+    private String ip;
+
+    /**
+     * 请求参数
+     */
+    private Object parameter;
+
+    /**
+     * 请求结果
+     */
+    private Object result;
+
+}
